b077d8fa7ae5c137081cdf9741816ecdcc563f4f
Paul <paulsudol@gmail.com>
2012-04-04 01:41:14 +0000

    Refactored some of the animation. Added random alpha fade.

--- particle.js
+++ particle.js
@@ -1,9 +1,9 @@
 
     //The number of particles on the screen at once
-    var PARTICLE_COUNT = 40;
+    var PARTICLE_COUNT = 50;
     
     //This array will hold all the circles to be rendered
-    var circles = new Array;
+    var circles = [];
     
     //The current timeout
     var t;
@@ -43,50 +43,60 @@
 	    context.clearRect(0, 0, canvas.width, canvas.height);
 	    
         //Draw all the cirlces
+        var currentCircle;
         for (currentCircle in circles) {
-            
             context.globalAlpha = circles[currentCircle].alpha;
-    	    context.beginPath();
-    	    context.moveTo(circles[currentCircle].centerX + circles[currentCircle].radius, circles[currentCircle].centerY);
-    	    context.arc(
-    	        circles[currentCircle].path[circles[currentCircle].currentPoint].x,
+            context.beginPath();
+            context.moveTo(circles[currentCircle].centerX + circles[currentCircle].radius, circles[currentCircle].centerY);
+            context.arc(
+                circles[currentCircle].path[circles[currentCircle].currentPoint].x,
                 circles[currentCircle].path[circles[currentCircle].currentPoint].y,
-    	        circles[currentCircle].radius,
-    	        circles[currentCircle].startingAngle,
-    	        circles[currentCircle].endingAngle,
-    	        circles[currentCircle].counterclockwise
-    	    );
+                circles[currentCircle].radius,
+                circles[currentCircle].startingAngle,
+                circles[currentCircle].endingAngle,
+                circles[currentCircle].counterclockwise
+            );
             
-    	    context.lineWidth = circles[currentCircle].lineWidth;
-    	    context.strokeStyle = circles[currentCircle].strokeStyle;
-    	    context.fillStyle = circles[currentCircle].fillStyle;
-    	    
-    	    context.fill();
+            context.lineWidth = circles[currentCircle].lineWidth;
+            context.strokeStyle = circles[currentCircle].strokeStyle;
+            context.fillStyle = circles[currentCircle].fillStyle;
+
+            context.fill();
             
             //Increment the point on the path
             circles[currentCircle].currentPoint++;
             
-            //Step down the alpha channel
-            circles[currentCircle].alpha -= .05;
+            //Decrement down the alpha channel
+            circles[currentCircle].alpha -= circles[currentCircle].alphaIncrement;
             
-            if (circles[currentCircle].currentPoint > (circles[currentCircle].path.length -1)) {
-                //If the circle does not have another point to move to, remove it
+            //Decrement the radius
+            circles[currentCircle].radius -= circles[currentCircle].radiusIncrement;
+            
+            /*
+             * If the circle does not have another point to move to, OR the
+             * circle is now invisible OR the radius is less that 1, remove it
+             */
+            if ((circles[currentCircle].currentPoint > (circles[currentCircle].path.length -1)) || 
+                (circles[currentCircle].alpha <= 0) ||
+                (circles[currentCircle].radius < 1)) {
                 circles.splice(currentCircle, 1);
-                if (isActive)
-                    circles.splice(0, 0, getCircle());
-            } else if (circles[currentCircle].alpha <= 0) {
-                //If the circle is now invisible, remove it
-                circles.splice(currentCircle, 1);
-                if (isActive)
-                    circles.splice(0, 0, getCircle());
             }
+
         }
-        
+    
+        if (isActive) {
+            while (circles.length < PARTICLE_COUNT) {
+                circles.splice(0, 0, getCircle());
+            }
+        } else if (circles.length <= 0) {
+            clearInterval(t);
+        }
         //If there are still circles left in the array, set a timer to redraw then
-        if (circles.length > 0)
-            t = setTimeout("drawCircle()", 25);
-        else
-            context.clearRect(0, 0, canvas.width, canvas.height);
+        
+            
+        //    t = setInterval(function (){drawCircle();}, 50);
+        //else
+        //    context.clearRect(0, 0, canvas.width, canvas.height);
 	}
     
     function getCircle() {
@@ -94,53 +104,55 @@
         var radius = Math.floor((Math.random()*8)+2);
         
         //Get a random direction
-        var direction = Math.floor((Math.random()*3600)+1);
+        var direction = Math.floor((Math.random()*360)+1);
         var radians = direction * (Math.PI/180);
         
+        //Get a random color from an array
         var colorOption = Math.floor(Math.random() * 3);
-    	var colorOptions = [
+        var colors = [
 		    "#00495E",
 		    "#00BFF7",
 		    "#2F2F2F"
 		];
 
         //Get the appropriate x and y increments
-        var xIncrement = Math.floor(Math.sin(radians) * 10);
-        var yIncrement = Math.floor(Math.cos(radians) * 10);
+        var xIncrement = Math.floor(Math.sin(radians) * 10)/4;
+        var yIncrement = Math.floor(Math.cos(radians) * 10)/4;
         
-        var path = new Array;
+        var path = [];
         
         var currentX = mousePos.x;
         var currentY = mousePos.y;
         
-        //first point on the path is the current mouse position
+        //The first point on the path is the current mouse position
         path.splice(0, 0, {"x" : currentX, "y" : currentY});
         
         //Put x more points on the path
-        for (var x=1; x<Math.floor((Math.random()*100)+1); x++){
+        var steps = Math.floor((Math.random()*200)+10);
+        for (var x=1; x<steps; x++) {
             currentX += xIncrement;
             currentY += yIncrement;
             var coord = {
                 "x": currentX,
-                "y": currentY,
+                "y": currentY
             };
             path.splice(x, 0, coord); 
         }
             
         var myCircle = {
-    		centerX: mousePos.x,
+            centerX: mousePos.x,
 			centerY: mousePos.y,
 			radius: radius,
+            radiusIncrement: Math.floor((Math.random()*20) + 10)/100,
 			startingAngle: 0 * Math.PI,
 	        endingAngle: 2 * Math.PI,
 	        counterclockwise: false,
-	        strokeStyle: "black",
 	        lineWidth: 1,
-	        fillStyle: colorOptions[colorOption],
-            alpha: 1,
+	        fillStyle: colors[colorOption],
+            alpha: 1,            
+            alphaIncrement: Math.floor((Math.random()*20) + 10)/1000,
             path: path,
-            currentPoint: 0,
-            incrementAlpha: 3
+            currentPoint: 0
 		};
         
         return myCircle;
@@ -148,7 +160,6 @@
     
     window.onload = function() {
         var canvas = document.getElementById('myCanvas');
-        var context = canvas.getContext('2d');
         
         canvas.addEventListener('mousemove', function(evt){
             mousePos = getMousePos(canvas, evt);
@@ -159,15 +170,11 @@
             mousePos = getMousePos(canvas, evt);
             isActive = true;
             
-            for (var x=0; x<PARTICLE_COUNT; x++) {
-                circles.splice(0, 0, getCircle());
-            }
-            
-            //clearTimeout(t);
-            drawCircle();
+            clearInterval(t);
+            t = setInterval(function (){drawCircle();}, 1000/30);
         }, false);
         
-        canvas.addEventListener('mouseout', function(evt){
+        canvas.addEventListener('mouseout', function(){
             isActive = false;
         }, false);
     };
